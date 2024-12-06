ec5a591da27f10300b00d1761457aaaff0ea025f
alyasultan <sultan.alla@phystech.edu>
2024-12-06 20:46:23 +0200

    feat(base-layout): add header for the application

---
+++ src/components/Header/Logo.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import classes from './Header.module.css'
+
+
+export const Logo: React.FC = () => {
+    return(
+        <div className={classes.logo}>
+            <img src="https://cdn-icons-png.flaticon.com/512/2960/2960590.png"  alt="Perfume Store" className={classes['logo-image']} />
+            <h1 className={classes['logo-text']}>GS Parfum</h1>
+        </div>
+    );
+};
